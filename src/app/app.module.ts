d388cfa66ca4dd8693efb73d88290a64395d30ae
Matt Huntington <matt.huntington@gmail.com>
2017-06-17 18:58:36 -0400

    generating films component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FilmsComponent } from './films/films.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FilmsComponent
   ],
   imports: [
     BrowserModule
